0b9da34856867cd76667585da9f38413ab8eaa00
jmclements <joseph.clements@viasat.com>
2019-06-17 14:26:07 -0700

    few fixes

--- README.md
+++ README.md
@@ -8,6 +8,48 @@ Flask based webservice for querying from your /etc/passwd and /etc/group files. 
 
 ## Quickstart Guide
 1. Clone this repo to your machine
-2. Run build.sh
-3. Run run.sh
+2. To build the service, run build.sh
+3. To start, run start.sh
 4. Query your local machine on port 5000, (e.g., curl http://127.0.0.1:5000/)
+5. (Cleanup) Shutdown/remove your docker container once you're through.
+
+For information on how to stop and remove docker containers, visit the following:
+https://docs.docker.com/engine/reference/commandline/stop/
+https://docs.docker.com/engine/reference/commandline/rm/
+
+# Config Options
+You may change the path of the passwd/group files the server queries from by passing them as arguments to the start script in the format "./start.sh $PASSWD_FILE $GROUP_FILE". For example, to change the passwd file to "/var/passwd/" and the group file to "/var/group" on the host machine, run "./start.sh /var/passwd /var/group". 
+
+Note that because these are positional arguments in bash, you must pass the both arguments in order to edit the group file path.
+
+# Unit test
+A simple unit test which tests the various endpoints will return 200 success has been included under "test/unit_test.py". To execute this, you must first prepare your python environment.
+
+1. Source the setup script (e.g., "source setup.sh").
+
+This will automatically create a virtual environment and install all necessary packages, as well as activate the environment.
+
+2. Make sure the docker container has been built and is running (see above Quickstart guide)
+3. Run the unit test script (E.g, ./tests/unit_test.py)
+
+## API endpoints
+- /users
+Returns a list of users from the passwd file.
+
+- /users/query[?name=<nq>][&uid=<uq>][&gid=<gq>][&comment=<cq>][&home=<hq>][&shell=<sq>]
+Returns a list of users matching the query arguments passed.
+
+- /users/<uid>
+Returns a single user matching the uid. Will return 404 if no such user exists.
+
+- /users/<uid>/groups
+Returns a list of the groups the user with matching the uid is a member of. Will return 404 if no such user exists.
+
+- /groups
+Returns a list of groups from the group file.
+
+- /groups/query[?name=<nq>][&gid=<gq>][&member=<mq1>][&member=<mq2>][&...]
+Returns a list of the groups matching the query arguments passed. Multiple member fields may be passed.
+
+- /groups/<gid>
+Returns a single group matching the gid. Will return 404 if no such group exists.
--- app/flaskwrd.py
+++ app/flaskwrd.py
@@ -1,5 +1,4 @@
 from flask import Flask
-#from app import app
 import flask
 import os
 import re
@@ -11,16 +10,18 @@ group_file = os.getenv('GROUP_FILE')
 
 @app.route('/')
 def home():
-    return("Flaskwrd server for querying from your passwd and group files. Passwd file: {}, group file: {}\n".format(passwd_file, group_file))
+    return("Flaskwrd server for querying from your passwd and group files.\n")
 
 @app.route('/users')
 def users():
+    check_passwd_format()
     users = parse_passwd()
     json_users = passwd_to_json(users)
     return(flask.jsonify(json_users))
 
 @app.route('/users/query')
 def users_query():
+    check_passwd_format()
     args = {}
     name_query = flask.request.args.getlist('name')
     uid_query = flask.request.args.getlist('uid')
@@ -46,6 +47,7 @@ def users_query():
 
 @app.route('/users/<uid>')
 def users_uid(uid):
+    check_passwd_format()
     users = parse_passwd(uid=uid)
     # if the list of users is empty, uid was not found, return 404
     if not users:
@@ -55,6 +57,7 @@ def users_uid(uid):
 
 @app.route('/users/<uid>/groups')
 def users_uid_groups(uid):
+    check_passwd_format()
     users = parse_passwd(uid=uid)
     # if the list of users is empty, uid was not found, return 404
     if not users:
@@ -66,12 +69,14 @@ def users_uid_groups(uid):
 
 @app.route('/groups')
 def groups():
+    check_group_format()
     groups = parse_group()
     groups_json = group_to_json(groups)
     return(flask.jsonify(groups_json))
 
 @app.route('/groups/query')
 def groups_query():
+    check_group_format()
     args = {}
     name_query = flask.request.args.getlist('name')
     gid_query = flask.request.args.getlist('gid')
@@ -105,6 +110,7 @@ def groups_query():
 
 @app.route('/groups/<gid>')
 def groups_gid(gid):
+    check_group_format()
     groups = parse_group(gid=gid)
     if not groups:
         flask.abort(404, 'No groups matching gid: {}'.format(gid))
@@ -123,7 +129,7 @@ def parse_group(name='.*', gid='.*', member='.*'):
         matches = re.findall(r'^{0}:.*:{1}:{2}$'.format(name, gid, member), open(group_file).read(), flags=re.MULTILINE)
         return(matches)
     except Exception as e:
-        flask.abort(500, "Group file does not exist")
+        flask.abort(500, "Group file does not exist. {}".format(str(e)))
 
 def passwd_to_json(passwd_list):
     out_list = []
@@ -151,6 +157,17 @@ def group_to_json(group_list):
         new_entry.update({'members': fields[3].split(",")})
         out_list.append(new_entry)
     return(out_list)
+
+
+def check_passwd_format():
+    users = parse_passwd()
+    if not users:
+        flask.abort(500, "No properly formatted passwd entries detected. Passwd file may be malformed")
+
+def check_group_format():
+    groups = parse_group()
+    if not groups:
+        flask.abort(500, "No properly formatted group entries detected. Group file may be malformed")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
